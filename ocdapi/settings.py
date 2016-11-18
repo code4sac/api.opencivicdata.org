94e100a28062dac33b4c58c5d38ea204833217f6
Eric van Zanten <eric.vanzanten@gmail.com>
2016-11-18 14:06:29 -0600

    Re-zip the zip

--- ocdapi/settings.py
+++ ocdapi/settings.py
@@ -13,7 +13,7 @@ def envvar(name, default=None):
 SECRET_KEY = envvar('SECRET_KEY', 'ITSASECRET')
 RAVEN_DSN = envvar('RAVEN_DSN', '')
 ALLOWED_HOSTS = envvar('ALLOWED_HOSTS', '*').split(',')
-DATABASES = {'default': dj_database_url.config(default='postgis://reginacompton:@localhost/opencivicdata')}
+DATABASES = {'default': dj_database_url.config(default='postgis://eric:@localhost/opencivicdata')}
 ELASTICSEARCH_HOST = envvar('ELASTICSEARCH_HOST', 'http://localhost:9200')
 TEMPLATE_DEBUG = DEBUG = envvar('DJANGO_DEBUG', 'False').lower() == 'true'
 USE_LOCKSMITH = envvar('USE_LOCKSMITH', 'false').lower() == 'true'
@@ -181,6 +181,3 @@ IMAGO_BOUNDARY_MAPPINGS = {
 }
 
 
-GDAL_LIBRARY_PATH = "/usr/local/lib/libgdal.dylib"
-import ctypes
-ctypes.CDLL(GDAL_LIBRARY_PATH)
--- shapefiles/state:ca/place:losangeles/committee_districts_2016/definition.py
+++ shapefiles/state:ca/place:losangeles/committee_districts_2016/definition.py
@@ -3,13 +3,9 @@ from datetime import date
 import boundaries
 
 def sector_namer(feature):
-    print("Running sector_name")
-    print(feature.get("Sector"))
     return 'la-metro-committee-district-{0}'.format(feature.get('Sector'))
 
 def ocd_id_func(feature):
-    print("Running ocd_id_func")
-    print(feature.get("Sector"))
     return 'ocd-division/country:us/state:ca/place:losangeles/district:{0}'.format(feature.get('Sector'))
 
 boundaries.register('la-metro-committee-districts',
@@ -30,9 +26,3 @@ boundaries.register('la-metro-committee-districts',
     label_point_func=lambda feature: None,
 )
 
-
-boundaries.register('Federal electoral districts',
-    encoding='iso-8859-1',
-    last_updated=date(2011, 11, 28),
-    name_func=boundaries.clean_attr('FEDENAME'),
-)
