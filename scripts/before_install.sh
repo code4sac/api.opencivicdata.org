57a887fabd9f86bf06f9ee1db28ae270ae00a141
Forest Gregg <fgregg@uchicago.edu>
2017-07-18 15:08:48 -0500

    before_install.sh needs to be unencrypted

---
+++ scripts/before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# Create a fresh project directory. (This is mainly
+# to ensure that these scripts work on a bare server.)
+rm -Rf /home/datamade/ocd-api
+mkdir -p /home/datamade/ocd-apib
+
+
+# Decrypt files encrypted with blackbox
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
